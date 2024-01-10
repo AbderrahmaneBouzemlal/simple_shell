0424e660972237970984b5e2384582b680a6d842
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-10 12:40:29 +0800

    Added a file

--- execute.c
+++ execute.c
@@ -6,54 +6,62 @@
  * @av: vector of arguments passed by to the shell
  * Return: 0 when it succeed and 1 otherwise
  */
+void execute_command(char *command, char **argv, char **env, char **av)
+{
+	pid_t child_pid;
+	int status;
+	
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		if (execve(command, argv, env) == -1)
+		{
+			perror(av[0]);
+			free(command);
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	else if (child_pid < 0)
+	{
+		perror(av[0]);
+		free(command);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		do
+		{
+			waitpid(child_pid, &status, WUNTRACED);
+		}
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		if (command != argv[0])
+		{
+			free(command);
+		}
+	}
+}
+
 int execute(char **argv, char **env, char **av)
 {
-	pid_t chpro;
-	int status;
-	char *command = NULL;
-
+	char *command;
 	if (argv == NULL || argv[0] == NULL)
 		return (0);
-
 	if (strcmp(argv[0], "env") == 0)
 	{
 		my_env(env);
 		return (0);
 	}
+
 	if (strcmp(argv[0], "exit") == 0)
-		return (my_exit(argv[1]));
+		return my_exit(argv[1]);
 
-	if (access(argv[0], F_OK) == 0)
-		command = argv[0];
-	else
-	{
-		command = find(argv[0]);
-		if (command == NULL)
-			return (127);
-	}
-	chpro = fork();
-	if (chpro == 0)
-	{
-		if (execve(command, argv, env) == -1)
-			perror(av[0]);
-		free(command);
-		exit(EXIT_FAILURE);
-	}
-	else if (chpro < 0)
-        {
-		perror(av[0]);
-		free(command);
-		exit(EXIT_FAILURE);
-        }
+	command = find(argv[0]);
 
-	else
-	{
-		do 
-		{
-			waitpid(chpro, &status, WUNTRACED);
-			if (command != argv[0])
-				free(command);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
+	if (command == NULL)
+		return (127);
+
+	execute_command(command, argv, env, av);
+
 	return (1);
 }
